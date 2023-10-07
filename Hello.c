7941b55c1064fb5819850c29617e19dfbbf69f30
yemiancheng <ymc-github@gmail.com>
2023-10-07 08:00:00 +0800

    feat(core): add hello world

---
+++ Hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// #include "Zero.h"
+int main()
+{
+    printf("[info] Hello, Zero! \n");
+    // use print method in Zero.h
+    // print();
+    // printf("[info] in vscode - in cmake - debug *.c files ! \n");
+    return 0;
+}
